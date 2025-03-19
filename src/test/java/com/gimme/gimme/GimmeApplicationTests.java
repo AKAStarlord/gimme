19d527fc110b79d12ab0ad10b2ccf28ec5e1a809
Tim Dale <timdale7@gmail.com>
2025-03-19 12:05:34 -0400

    initial commit

---
+++ src/test/java/com/gimme/gimme/GimmeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gimme.gimme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("you did it!");
+	}
+
+}
